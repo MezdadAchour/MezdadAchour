086dcab4992f69c8024e852f814fc2cdc9936157
MezdadAchour <m.achour2022@gmail.com>
2024-11-03 15:12:27 +0100

    var

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
+// Étendre les types globaux de Node.js
 declare global {
-    let prisma: PrismaClient | undefined; // Utiliser 'let' pour la variable globale
+    var prisma: PrismaClient | undefined; // Utiliser 'var' pour la variable globale
 }
 
 // Initialiser le client Prisma
-const prisma = global.prisma || new PrismaClient({
+const prisma = globalThis.prisma || new PrismaClient({
     log: ['query', 'error', 'warn'],
 });
 
 // Ne pas conserver la référence globale en production
 if (process.env.NODE_ENV !== 'production') {
-    global.prisma = prisma; // Assurez-vous que cette ligne ne sera pas exécutée en production
+    globalThis.prisma = prisma; // Assurez-vous que cette ligne ne sera pas exécutée en production
 }
 
 // Exporter le client Prisma

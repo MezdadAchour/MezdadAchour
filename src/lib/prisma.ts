a24d1139fe48cd4156a2d8296bc08c1d918d783d
MezdadAchour <m.achour2022@gmail.com>
2024-11-03 14:11:57 +0100

    let

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,18 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
 declare global {
-    var prisma: PrismaClient | undefined
+    let prisma: PrismaClient | undefined; // Utiliser 'let' pour la variable globale
 }
 
+// Initialiser le client Prisma
 const prisma = global.prisma || new PrismaClient({
     log: ['query', 'error', 'warn'],
-})
+});
 
+// Ne pas conserver la référence globale en production
 if (process.env.NODE_ENV !== 'production') {
-    global.prisma = prisma
+    global.prisma = prisma; // Assurez-vous que cette ligne ne sera pas exécutée en production
 }
 
-export { prisma }
+// Exporter le client Prisma
+export { prisma };
